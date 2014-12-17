75b202850ae0fc2b6eda407ef8102317ff801abe
Paul Rouget <me@paulrouget.com>
2014-12-17 16:42:35 +0100

    show a warning if require.js is missing

---
+++ js/missingrequirewarning.js
@@ -0,0 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+window.addEventListener('load', () => {
+  if (!('require' in window)) {
+    alert(`Missing require.js library.
+Did you forget to pull the submodules?
+Type: git submodule update --init`);
+  }
+});
