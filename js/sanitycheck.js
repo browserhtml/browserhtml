e0149d659068a8d55a31757e83a5216ae33b9462
Paul Rouget <me@paulrouget.com>
2014-12-17 17:04:31 +0100

    do not make the startup check requirejs-specific

--- js/sanitycheck.js
+++ js/sanitycheck.js
@@ -3,9 +3,13 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 window.addEventListener('load', () => {
+
+  // require.js
+
   if (!('require' in window)) {
     alert(`Missing require.js library.
 Did you forget to pull the submodules?
 Type: git submodule update --init`);
   }
+
 });
