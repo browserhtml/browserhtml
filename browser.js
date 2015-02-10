fd5a7b5d8e64defa8d4d45b721d1903deb09d4a2
Irakli Gozalishvili <rfobic@gmail.com>
2015-02-09 16:04:22 -0800

    Setup a foundation.

---
+++ browser.js
@@ -0,0 +1,33 @@
+require.config({
+  scriptType: 'text/javascript;version=1.8',
+  baseUrl: 'node_modules/',
+  nodeIdCompat: true,
+  paths: {
+    // http://facebook.github.io/react/
+    'react': 'react/dist/react',
+    // http://facebook.github.io/immutable-js/
+    // Because of the bug https://github.com/facebook/immutable-js/pull/297
+    // we use forked version until it's uplifted.
+    'immutable': 'immutable/dist/immutable',
+    'immutable/cursor': 'immutable/contrib/cursor/index',
+    // http://omniscientjs.github.io
+    'omniscient': 'omniscient/dist/omniscient',
+    // https://github.com/broofa/node-uuid
+    'uuid': 'node-uuid/uuid'
+  },
+  shim: {
+    'immutable/cursor': {
+      exports: 'Cursor',
+      deps: ['shims/immutable-cursor', 'immutable']
+    },
+    'omniscient': {
+      deps: ['shims/omniscient']
+    },
+    'pouchdb': {
+      exports: 'PouchDB'
+    }
+  }
+});
+
+
+require(['browser/index']);
