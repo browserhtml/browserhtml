aea7fc82f73a800495b86319c2333dc524f1ddaa
Paul Rouget <me@paulrouget.com>
2015-02-18 10:31:58 +0100

    typo

--- src/browser/actions.js
+++ src/browser/actions.js
@@ -16,8 +16,8 @@ const readInputURL = input =>
   !url.hasScheme(input) ? `http://${input}` :
   input;
 
-// Action takes state cursor for the web viwer and input location
-// and navigates that webViewr to that location (if it's not valid
+// Action takes state cursor for the web viewer and input location
+// and navigates that webViewer to that location (if it's not valid
 // url either normalizes it or converts to search). Optional `focus`
 // can be passed as `false` to navigate to a url but not focus it.
 const navigateTo = ({input, webViewer}, location, focus=true) => {
