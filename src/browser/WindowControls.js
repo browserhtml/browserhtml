3278ee26fe2a3bd32a04bbfbeef810e83503ad93
Paul Rouget <me@paulrouget.com>
2015-04-13 07:46:52 +0200

    new WindowControl component

---
+++ src/browser/WindowControls.js
@@ -0,0 +1,61 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+define((require, exports, module) => {
+
+  'use strict';
+
+  const Component = require('omniscient');
+  const {DOM} = require('react')
+  const {sendEventToChrome} = require('./actions');
+
+  const styleContainer = {
+    alignItems: 'center',
+    flexShrink: 0,
+    marginLeft: 7
+  };
+
+  const styleButton = {
+    width: 12,
+    height: 12,
+    marginRight: 8,
+    borderRadius: '50%'
+  };
+
+  const styleGreyButton = Object.assign({
+    backgroundColor: 'hsl(0, 0%, 86%)'
+  }, styleButton);
+
+  const WindowControls = Component(({isDocumentFocused, windowControls, theme}) => {
+
+    const styleMinButton = Object.assign({}, theme.windowMinButton, styleButton);
+    const styleMaxButton = Object.assign({}, theme.windowMaxButton, styleButton);
+    const styleCloseButton = Object.assign({}, theme.windowCloseButton, styleButton);
+
+    return DOM.div({
+      key: 'WindowControlsContainer',
+      style: styleContainer,
+    }, [
+      DOM.div({
+        key: 'WindowCloseButton',
+        style: isDocumentFocused ? styleCloseButton : styleGreyButton,
+        onClick: e => sendEventToChrome('shutdown-application')
+      }, {
+        }),
+      DOM.div({
+        key: 'WindowMinButton',
+        style: isDocumentFocused ? styleMinButton : styleGreyButton,
+        onClick: e => sendEventToChrome('minimize-native-window')
+      }),
+      DOM.div({
+        key: 'WindowMaxButton',
+        style: isDocumentFocused ? styleMaxButton : styleGreyButton,
+        onClick: e => sendEventToChrome('toggle-fullscreen-native-window')
+      })
+    ])
+  });
+
+  exports.WindowControls = WindowControls;
+
+});
