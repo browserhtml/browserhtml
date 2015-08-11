b5a52845725b1ddb37d24e97020bb9b6a498016a
Irakli Gozalishvili <rfobic@gmail.com>
2015-08-10 17:16:24 -0700

    Make settings editable.

--- src/about/settings/index.js
+++ src/about/settings/index.js
@@ -8,6 +8,33 @@
   const {Record, Any} = require('../../common/typed');
   const {Map} = require('immutable');
   const Settings = require('../../service/settings');
+  const React = require('react');
+
+
+  class Input extends React.Component {
+    componentDidUpdate(props) {
+      const value = this.props.text
+      if (value != props.text) {
+        const node = React.findDOMNode(this);
+        const start = node.selectionStart;
+        const end = node.selectionEnd;
+
+        node.value = value;
+        node.setSelectionRange(start, end);
+      }
+    }
+    componentDidMount () {
+      if (this.props.text) {
+        const node = React.findDOMNode(this);
+        node.value = this.props.text;
+      }
+    }
+    render(props, state) {
+      return React.createElement('input', this.props);
+    }
+  };
+
+  const input = props => React.createElement(Input, props);
 
   // Model
 
@@ -28,7 +55,99 @@
 
   // View
 
-  const viewSetting = (name, value) => html.div({
+  const viewField = props => {
+    return html.span({
+      key: 'field',
+      style: {
+        position: 'relative',
+        overflow: 'hidden'
+      }
+    },[
+      html.span({
+        key: 'sizer',
+        style: {
+          left: 0,
+          color: 'red',
+          whiteSpace: 'pre',
+          opacity: 0,
+        }
+      }, props.value),
+      input({
+        style: {
+          padding: 0,
+          margin: 0,
+          background: 'transparent',
+          border: 'none',
+          font: 'inherit',
+          margin: 0,
+          padding: 0,
+          position: 'absolute',
+          zIndex: 2,
+          left: 0,
+          width: '100%'
+        },
+        type: 'text',
+        text: props.value,
+        onChange: props.onChange
+      })
+    ]);
+  };
+
+  const viewValue = (name, value, address) => {
+    const type = value == null ? 'null' :
+          typeof(value);
+
+    return type === 'string' ? viewString(name, value, address) :
+           type === 'number' ? viewNumber(name, value, address) :
+           type === 'boolean' ? viewBoolean(name, value, address) :
+           viewJSON(name, value, address);
+  }
+
+  const viewNumber = (name, value, address) =>
+    html.input({
+      type: 'number',
+      style: {
+        border: 'none',
+        width: 'auto'
+      },
+      value,
+      onChange: event => address.receive(Settings.Update({
+        name, value: event.target.valueAsNumber
+      }))});
+
+  const viewBoolean = (name, value, address) =>
+    html.input({
+      type: 'checkbox',
+      checked: value,
+      onChange: event => address.receive(Settings.Update({
+        name, value: event.target.checked
+      }))});
+
+  const viewString = (name, value, address) =>
+    html.code(null, [
+      '"',
+      viewField({
+        value,
+        onChange: event => address.receive(Settings.Update({
+          name,
+          value: event.target.value
+        }))}),
+      '"'
+    ]);
+
+  const viewJSON = (name, value, address) =>
+    viewField({
+      value,
+      onChange: event => address.receive(Settings.Update({
+        name,
+        value: event.target.value === 'true' ? true :
+               event.target.value === 'false' ? false :
+               event.target.value === 'null' ? null :
+               !Number.isNaN(Number(event.target.value)) ? Number(event.target.value) :
+               event.target.value
+      }))});
+
+  const viewSetting = (name, value, address) => html.div({
     className: 'row'
   }, [
     html.span({
@@ -40,14 +159,14 @@
       key: 'value',
       className: 'value',
       title: value,
-    }, JSON.stringify(value))
+    }, [ viewValue(name, value, address)])
   ]);
 
-  const view = state => html.div({
+  const view = (state, address) => html.div({
     key: 'table',
     className: 'table'
   }, state.settings.map((value, key) => {
-    return render(key, viewSetting, key, value);
+    return render(key, viewSetting, key, value, address);
   }).values());
   exports.view = view;
 
