30ee0236c12e0c590e4dc72abdaf9a14723e6603
Irakli Gozalishvili <rfobic@gmail.com>
2015-08-21 21:25:33 -0700

    Get rid of fancy editing features.

--- src/about/settings/index.js
+++ src/about/settings/index.js
@@ -5,38 +5,12 @@
   'use strict';
 
   const {render, html, Address, Application} = require('reflex');
-  const {Record, Any} = require('typed-immutable');
+  const {Record, Any, Maybe, Union} = require('typed-immutable');
   const {Map} = require('immutable');
   const Settings = require('../../service/settings');
-  const React = require('react');
   const {StyleSheet, Style} = require('../../common/style');
   const ClassSet = require('../../common/class-set');
 
-
-  class Input extends React.Component {
-    componentDidUpdate(props) {
-      const value = this.props.text
-      if (value != props.text) {
-        const node = React.findDOMNode(this);
-        const start = node.selectionStart;
-        const end = node.selectionEnd;
-
-        node.value = value;
-        node.setSelectionRange(start, end);
-      }
-    }
-    componentDidMount () {
-      if (this.props.text) {
-        const node = React.findDOMNode(this);
-        node.value = this.props.text;
-      }
-    }
-    render(props, state) {
-      return React.createElement('input', this.props);
-    }
-  };
-
-  const input = props => React.createElement(Input, props);
 
   // Model
 
@@ -45,17 +19,73 @@
   }, 'Settings');
   exports.Model = Model;
 
+  const PendingChange = Record({
+    isEditing: true,
+    value: Maybe(Union(String, Number, Boolean)),
+    input: String
+  }, 'PendingChange');
+
   // Update
 
+  const StartEdit = Record({
+    name: String
+  }, 'StartEdit');
+
+  const StopEdit = Record({
+    name: String
+  }, 'StopEdit');
+
+  const CancelEdit = Record({
+    name: String,
+  }, 'CancelEdit');
+
   const Edit = Record({
-    edit: true,
     name: String,
     input: String
   }, 'Edit');
 
+  const Action = Union(StartEdit, StopEdit, CancelEdit, Edit);
+
+  const startEdit = (settings, name) => {
+    const change = settings.get(name);
+    return change instanceof PendingChange ?
+      settings.set(name, change.set('isEditing', true)) :
+      settings.set(name, PendingChange({
+        value: change,
+        input: JSON.stringify(change)
+      }));
+  }
+
+  const stopEdit = (settings, name) => {
+    const change = settings.get(name);
+    return change instanceof PendingChange ?
+      settings.setIn([name, 'isEditing'], false) :
+      settings;
+  }
+
+  const cancelEdit = (settings, name) => {
+    const change = settings.get(name);
+    return change instanceof PendingChange ?
+      settings.set(name, change.value) :
+      settings;
+  }
+
+  const edit = (settings, name, input) => {
+    const change = settings.get(name);
+    return change instanceof PendingChange ?
+      settings.setIn([name, 'input'], input) :
+      settings;
+  }
+
   const update = (state, action) =>
+    action instanceof StartEdit ?
+      state.set('settings', startEdit(state.settings, action.name)) :
+    action instanceof StopEdit ?
+      state.set('settings', stopEdit(state.settings, action.name)) :
+    action instanceof CancelEdit ?
+      state.set('settings', cancelEdit(state.settings, action.name)) :
     action instanceof Edit ?
-      state.setIn(['settings', action.name], action) :
+      state.set('settings', edit(state.settings, action.name, action.input)) :
     action instanceof Settings.Changed ?
       state.setIn(['settings', action.name], action.value) :
     action instanceof Settings.Fetched ?
@@ -65,92 +95,110 @@
 
   // View
 
-  const fieldStyle = StyleSheet.create({
-    // input field is wrapped in a box so that input field itself
-    // will resize to match it's container & subsequently typed text.
-    // input field is positioned absolute so it's size won't affect
-    // container & it's styles are blanked out as container is supposed
-    // to be styled instead.
-    input: {
-      padding: 0,
-      margin: 0,
-      background: 'transparent',
-      border: 'none',
-      font: 'inherit',
+  const style = StyleSheet.create({
+    edit: {
+      fontFamily: 'inherit',
+      fontSize: 'inherit',
       color: 'inherit',
-      position: 'absolute',
-      zIndex: 2,
-      left: 0,
-      width: '100%'
+      width: 'calc(100% - 10px)',
+      border: 'none',
+      backgroundColor: 'rgba(255,255,255,0.2)',
+      color: 'rgba(255,255,255,0.7)',
+      borderRadius: '5px 5px 5px 5px',
+      padding: 5
     },
-    // Container box is positioned relative so that contaned input that
-    // is positioned absolute will be relative to this container. Overflow is
-    // also hidden so that input with in it will get clipped if it is too large.
-    box: {
-      position: 'relative',
-      overflow: 'hidden'
-    },
-    // Container box content element is used to only to resize a container
-    // based on the text of the input field that is also a text for the
-    // content element.
-    content: {
-      font: 'inherit',
-      left: 0,
-      podding: 0,
-      margin: 0,
-      whiteSpace: 'pre',
-      opacity: 0,
+    error: {
+      textDecoration: 'underline wavy red'
     },
     number: {
-      background: 'transparent',
       border: 'none',
+      backgroundColor: 'inherit',
       color: 'inherit'
-    }
-  });
+    },
+    boolean: {
+      cursor: 'pointer',
+      textDecoration: 'underline'
+    },
+    table: {
+      fontSize: 12,
+      fontFamily: 'Menlo, Courier, monospace',
+      color: 'rgba(255,255,255,0.65)',
+      backgroundColor: '#273340',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100vw',
+    },
+    row: {
+      borderBottom: '1px dotted rgba(255, 255, 255, 0.2)',
+      lineHeight: '25px',
+      whiteSpace: 'nowrap',
+      padding: '0 5px'
+    },
+    cell: {
+      verticalAlign: 'middle',
+      display: 'inline-block',
+      whiteSpace: 'nowrap',
+      textOverflow: 'ellipsis',
+      overflow: 'hidden'
+    },
+    name: {
+      minWidth: 300
+    },
+    value: {
+      width: '100vh',
+      padding: '0 0 0 0'
+    },
 
-  const viewField = props => {
-    return html.span(Object.assign({}, props, {
-      key: 'field',
-      style: fieldStyle.box,
-    }),[
-      html.span({
-        key: 'sizer',
-        style: fieldStyle.content,
-      }, props.value),
-      input(Object.assign({}, props, {
-        style: fieldStyle.input,
-        type: 'text',
-        text: props.value,
-        onChange: props.onChange
-      }))
-    ]);
-  };
+  });
 
   const viewValue = (name, value, address) => {
     const type = value == null ? 'null' :
+          value instanceof PendingChange ? 'pending' :
           typeof(value);
 
-    return type === 'number' ? viewNumber(name, value, address) :
-           //type === 'boolean' ? viewBoolean(name, value, address) :
+    return type === 'pending' ? viewPending(name, value, address) :
+           type === 'number' ? viewNumber(name, value, address) :
+           type === 'boolean' ? viewBoolean(name, value, address) :
            viewJSON(name, value, address);
   }
 
   const viewNumber = (name, value, address) =>
     html.input({
       type: 'number',
-      style: fieldStyle.number,
+      style: style.number,
       value,
-      onChange: event => address.receive(Settings.Update({
-        name, value: event.target.valueAsNumber
-      }))});
+      onClick: event => {
+        if (event.altKey) {
+          address.receive(StartEdit({name}));
+        }
+      },
+      onChange: event => {
+        address.receive(Settings.Update({
+          name, value: event.target.valueAsNumber
+        }));
+      }
+    });
 
   const viewBoolean = (name, value, address) =>
-    html.input({
-      type: 'checkbox',
-      checked: value,
-      onChange: event => address.receive(Settings.Update({
-        name, value: event.target.checked
-      }))});
+    html.code({
+      style: style.boolean,
+      onClick: event => {
+        if (event.altKey) {
+          address.receive(StartEdit({name}));
+        } else {
+          address.receive(Settings.Update({name, value: !value}));
+        }
+      }
+    }, JSON.stringify(value));
+
+  const viewJSON = (name, value, address) =>
+    html.code({
+      style: style.json,
+      onClick: event => {
+        address.receive(StartEdit({name}));
+      }
+    }, JSON.stringify(value));
 
   const parseInput = input => {
     try {
@@ -160,23 +208,48 @@
     }
   }
 
-  const viewJSON = (name, value, address) =>
-    viewField({
-      value: (value && value.edit) ? value.input :
-             JSON.stringify(value),
+  const viewPending = (name, change, address) =>
+    change.isEditing ? viewEdit(name, change, address) :
+    viewDirty(name, change, address);
+
+  const viewDirty = (name, {input}, address) =>
+    html.code({
+      style: style.error,
       onClick: event => {
-        if (value === true) {
-          address.receive(Settings.Update({name, value: false}));
+        address.receive(StartEdit({name}))
+      }
+    }, input);
+
+  const viewEdit = (name, {input, value, isEditing}, address) =>
+    html.input({
+      type: 'text',
+      style: Style(style.edit,
+                   parseInput(input) instanceof Error && style.error),
+      value: input,
+      onKeyUp: event => {
+        if (event.key === 'Enter') {
+          const result = parseInput(event.target.value);
+
+          if (result instanceof Error) {
+            address.receive(StopEdit({name}));
+          } else if (result === value) {
+            address.receive(CancelEdit({name}));
+          } else {
+            address.receive(Settings.Update({name, value: result}));
+          }
         }
 
-        if (value === false) {
-          address.receive(Settings.Update({name, value: true}));
+        if (event.key === 'Escape') {
+          address.receive(CancelEdit({name}));
         }
       },
       onChange: event => {
+        address.receive(Edit({name, input: event.target.value}));
+      },
+      onBlur: event => {
         const value = parseInput(event.target.value);
         if (value instanceof Error) {
-          address.receive(Edit({name, input: event.target.value}));
+          address.receive(StopEdit({name}));
         } else {
           address.receive(Settings.Update({name, value}));
         }
@@ -184,40 +257,31 @@
     });
 
   const viewSetting = (name, value, address) => html.div({
-    className: 'row'
+    style: style.row
   }, [
-    html.span({
+    html.code({
       key: 'name',
-      className: 'name',
+      style: Style(style.cell, style.name),
       title: name
     }, name),
-    html.span({
+    html.code({
       key: 'value',
-      className: ClassSet({
-        'value': true,
-        'cm-number': typeof(value) === 'number',
-        'cm-string': typeof(value) === 'string',
-        'cm-boolean': typeof(value) === 'boolean',
-        'cm-atom': value == null ||
-                   value === true ||
-                   value === false,
-        'cm-error': value && value.edit
-      }),
+      style: Style(style.cell, style.value),
       title: value,
-    }, [ viewValue(name, value, address)])
+    }, viewValue(name, value, address))
   ]);
 
   const view = (state, address) => html.div({
     key: 'table',
-    className: 'table'
+    style: style.table
   }, [
-    html.meta({
-      name: 'theme-color',
-      content: '002b36|839496',
-    }),
     ...state.settings.map((value, key) => {
       return render(key, viewSetting, key, value, address);
-    }).values()
+    }).values(),
+    html.meta({
+      name: 'theme-color',
+      content: `${style.table.backgroundColor}|${style.table.color}`
+    })
   ]);
   exports.view = view;
 
