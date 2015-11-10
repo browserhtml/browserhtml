c0d4c65fe50ce9fd4c9f725304f7db58c7a64ab1
Gordon Brander <gordonbrander@users.noreply.github.com>
2015-11-10 10:57:20 -0800

    Fix more of the missing imports

--- src/browser/browser.js
+++ src/browser/browser.js
@@ -9,34 +9,34 @@ import * as Assistant from "./assistant"
 
 import * as Updater from "./updater"
 // import * as Devtools from "./devtools"
-// import * as WebViews from "./web-views"
-//
-// import {asFor} from "../common/prelude"
-//
-// /*:: import * as type from "../../type/browser/browser" */
-//
-// export const initialize/*:type.initialize*/ = () => {
-//   const [devtools, devtoolsFx] = Devtools.initialize()
-//   const [updates, updaterFx] = Updater.initialize()
-//
-//   const model = {
-//     version,
-//     shell: Shell.initial,
-//     input: Input.initial,
-//     suggestions: Assistant.initial,
-//     webViews: WebViews.initial,
-//
-//     updates: updates,
-//     devtools: devtools
-//   }
-//
-//   const fx = Effects.batch([
-//     asFor("Devtools", devtoolsFx),
-//     asFor("Updater", updaterFx)
-//   ])
-//
-//   return [model, fx]
-// }
+import * as WebViews from "./web-views"
+
+import {asFor} from "../common/prelude"
+
+/*:: import * as type from "../../type/browser/browser" */
+
+export const initialize/*:type.initialize*/ = () => {
+  const [devtools, devtoolsFx] = Devtools.initialize();
+  const [updates, updaterFx] = Updater.initialize();
+
+  const model = {
+    version,
+    shell: Shell.initial,
+    input: Input.initial,
+    suggestions: Assistant.initial,
+    webViews: WebViews.initial,
+
+    // updates: updates,
+    // devtools: devtools
+  };
+
+  const fx = Effects.batch([
+    asFor("Devtools", devtoolsFx),
+    asFor("Updater", updaterFx)
+  ]);
+
+  return [model, fx]
+}
 //
 // export const step/*:type.step*/ = (model, address) => {
 //
---
+++ src/browser/index.js
@@ -0,0 +1,37 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+'use strict';
+
+const {start} = require('reflex');
+const Browser = require('./browser');
+// const PerspectiveUI = require('./perspective-ui');
+// const Session = require('./session');
+const version = require('../../package.json').version;
+const {Renderer} = require('reflex-virtual-dom-driver');
+
+const application = start({
+  initial: Browser.Model(window.application.model.value),
+  update: Browser.update,
+  view: (model, _) => Browser.view(model, address)
+});
+
+application.unload = () => application.address(Runtime.LiveReload());
+
+// If hotswap change address so it points to a new mailbox &
+// re-render.
+if (isReload) {
+  window.application.unload();
+  //application.render();
+}
+
+const renderer = new Renderer({target: document.body})
+application.view.subscribe(renderer.address)
+
+const address = action => {
+  // @TODO
+}
+
+// @TODO hook up services to address.
+
+window.application = application;
