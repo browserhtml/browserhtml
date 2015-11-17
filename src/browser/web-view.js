e4837b3561a9c5bc17c9a1e009b6a33271ab626c
Gordon Brander <gordonbrander@users.noreply.github.com>
2015-11-17 10:09:39 -0800

    Stub out web-view view

--- src/browser/web-view.js
+++ src/browser/web-view.js
@@ -6,7 +6,8 @@
 
 /*:: import * as type from "../../type/browser/web-view" */
 
-import {Effects} from 'reflex';
+import {Effects, html} from 'reflex';
+import {on, onCanGoBackChange, onCanGoForwardChange} from 'driver';
 import {updateIn, stepIn} from '../common/lang/object';
 import * as Shell from './web-view/shell';
 import * as Progress from './web-view/progress';
@@ -64,3 +65,64 @@ export const step/*:type.step*/ (model, action) =>
 
   // Default
   [model, Effects.none];
+
+const viewFrame = (model, address) =>
+  html.iframe({
+    id: `web-view-${loader.id}`,
+    src: location,
+    'data-uri': loader.uri,
+    // opener: opener(loader.opener),
+    // style: style.frame,
+    mozbrowser: true,
+    remote: true,
+    // mozapp: URI.isPrivileged(location) ? URI.getManifestURL().href : void(0),
+    mozallowfullscreen: true
+    // isVisible: visiblity(isSelected || !thumbnail),
+    // zoom: zoom(shell.zoom),
+    // navigation: navigate(navigation.state),
+    // isFocused: focus(shell.isFocused),
+    // onBlur: on(address, decodeBlur),
+    // onFocus: on(address, decodeFocus),
+    // onMozbrowserAsyncScroll: on(address, decodeAsyncScroll),
+
+    // onMozBrowserCanGoBackChange: onCanGoBackChange(address, decodeCanGoBackChange),
+    // onMozBrowserCanGoForwardChange: onCanGoForwardChange(address, decodeCanGoForwardChange),
+
+    // onMozBrowserClose: on(address, decodeClose),
+    // onMozBrowserOpenWindow: on(address, decodeOpenWindow),
+    // onMozBrowserOpenTab: on(address, decodeOpenTab),
+    // onMozBrowserContextMenu: on(address, decodeContexMenu),
+    // onMozBrowserError: on(address, decodeError),
+    // onMozBrowserLoadStart: on(address, decodeLoadStart),
+    // onMozBrowserLoadEnd: on(address, decodeLoadEnd),
+    // onMozBrowserFirstPaint: on(address, decodeFirstPaint),
+    // onMozBrowserDocumentFirstPaint: on(address, decodeDocumentFirstPaint),
+    // // onMozBrowserLoadProgressChange: on(address, decodeProgressChange),
+    // onMozBrowserLocationChange: on(address, decodeLocationChange),
+    // onMozBrowserMetaChange: on(address, decodeMetaChange),
+    // onMozBrowserIconChange: on(address, decodeIconChange),
+    // onMozBrowserLocationChange: on(address, decodeLocationChange),
+    // onMozBrowserSecurityChange: on(address, decodeSecurityChange),
+    // onMozBrowserTitleChange: on(address, decodeTitleChange),
+    // onMozBrowserShowModalPrompt: on(address, decodeShowModalPrompt),
+    // onMozBrowserUserNameAndPasswordRequired: on(address, decodeAuthenticate),
+    // onMozBrowserScrollAreaChanged: on(address, decodeScrollAreaChange),
+  });
+
+export const view/*:type.view*/ = (model, address) =>
+  html.div({
+    className: 'webview',
+  }, [
+    viewFrame(model, address),
+    html.div({className: 'webview-local-overlay'}),
+    html.div({className: 'webview-topbar'}, [
+      html.div({className: 'webview-topbar-background'}),
+      html.div({className: 'webview-combobar'}, [
+        html.div({className: 'webview-title-container'}, [
+          html.span({className: 'webview-security-icon'}),
+          html.span({className: 'webview-title'})
+        ])
+      ]),
+      html.div({className: 'webview-show-sidebar-button'})
+    ])
+  ]);
