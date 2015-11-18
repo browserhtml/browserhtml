0eda7c78cef31c8800b8c35c7574f99ee3dde5dc
Gordon Brander <gordonbrander@users.noreply.github.com>
2015-11-18 13:24:20 -0800

    Add readTitle to history type

--- src/common/history.js
+++ src/common/history.js
@@ -3,6 +3,8 @@
 /*:: import * as type from "../../type/common/history" */
 import {Task, Effects} from "reflex"
 
+export const readTitle/*:type.readTitle*/ = (model, fallback) =>
+  model.title ? model.title : fallback;
 
 export const query/*:type.query*/ = (input, limit) =>
   Effects.task(Task.io(deliver => {
