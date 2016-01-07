33781930ff1cd342b76804f7b562f41ccefa5ccd
Irakli Gozalishvili <rfobic@gmail.com>
2016-01-07 11:21:28 -0800

    As per f0c03c9.

--- src/browser/browser.js
+++ src/browser/browser.js
@@ -539,9 +539,6 @@ const styleSheet = StyleSheet.create({
   root: {
     background: '#24303D',
     perspective: '1000px',
-    // @TODO this isn't actually doing what we want (centering the webviews
-    // in the available space). We need to do some 3d space math.
-    perspectiveOrigin: 'calc(50% - 380px)',
     // These styles prevent scrolling with the arrow keys in the root window
     // when elements move outside of viewport.
     // @WORKAROUND Use percent, not vw and vh to work around
