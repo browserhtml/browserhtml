b28af108d05cd9a6156b63b4267f8eabc1b87d35
Gordon Brander <gordonbrander@users.noreply.github.com>
2016-01-07 10:26:45 -0800

    Remove animation code, just show/hide tab close

--- src/browser/sidebar/tab.js
+++ src/browser/sidebar/tab.js
@@ -1,94 +1,26 @@
 import {html, thunk, Effects} from 'reflex';
-import * as Easing from "eased";
 import {Style, StyleSheet} from '../../common/style';
-import * as Stopwatch from "../../common/stopwatch";
 import {cursor, merge} from "../../common/prelude";
 import * as Target from "../../common/target";
 import * as Unknown from "../../common/unknown";
 import * as WebView from '../web-view';
 
-export const Model
-  = ({isPointerOver}) =>
-  ({isPointerOver
-  , animation: null
-  , display
-      : isPointerOver
-      ? {opacity: 1}
-      : {opacity: 0}
-  });
+export const Model = ({isPointerOver}) =>
+  ({isPointerOver});
 
 export const initial = Model({isPointerOver: false});
 
 const Point = action => ({type: "Point", action});
-const Animation = action => ({type: "Animation", action});
-
-const stopwatch = cursor({
-  tag: Animation,
-  get: model => model.animation,
-  set: (model, animation) => merge(model, {animation}),
-  update: Stopwatch.step
-});
 
 const point = cursor({
+  get: (model) => model.tab,
+  set: (model, tab) => merge(model, {tab}),
   update: Target.step,
   tag: Point
 });
 
-// @TODO handle tab width and title opacity animations here
-// instead of in Sidebar.
-const interpolate = (from, to, progress) => merge(from, {
-  opacity: Easing.float(from.opacity, to.opacity, progress),
-});
-
-const animationProjection = model =>
-    model.isPointerOver
-  ? {opacity: 1}
-  : {opacity: 0};
-
-const animate = (model, action) => {
-  const [{animation}, fx] = stopwatch(model, action.action)
-  const duration = 200;
-
-  // @TODO: We should not be guessing what is the starnig point
-  // that makes no sense & is likely to be incorrect at a times.
-  // To fix it we need to ditch this easing library in favor of
-  // something that will give us more like spring physics.
-  const begin
-    = isPointerOver
-    ? {opacity: 0}
-    : {opacity: 1};
-
-  const projection = animationProjection(model);
-
-  return duration > animation.elapsed
-    ? [ merge(model, {
-          animation,
-          display: Easing.ease
-            ( Easing.easeOutCubic
-            , interpolate
-            , begin
-            , projection
-            , duration
-            , animation.elapsed
-            )
-        })
-      , fx
-      ]
-    : [ merge(model, {animation, display: projection})
-      , fx.map(AnimationEnd)
-      ]
-}
-
 export const update = (model, action) =>
-    action.type === "Animation"
-  ? animate(model, action)
-  : action.type === "AnimationEnd"
-  ? stopwatch(model, Stopwatch.End)
-  : action.type === "Show"
-  ? stopwatch(merge(model, {isOpen: true}), Stopwatch.Start)
-  : action.type === "Hide"
-  ? stopwatch(merge(model, {isOpen: false}), Stopwatch.Start)
-  : action.type === "Point"
+    action.type === "Point"
   ? point(model, action.action)
   : Unknown.step(model, action);
 
@@ -171,13 +103,13 @@ const style = StyleSheet.create({
   }
 });
 
-const viewClose = ({isSelected, isPointerOver}, address) =>
+const viewClose = ({isSelected, tab}, address) =>
   html.div({
     className: 'tab-close-mask',
     style: Style(
       style.closeMask,
       isSelected && style.closeMaskSelected,
-      !isPointerOver && style.closeMaskHidden
+      !tab.isPointerOver && style.closeMaskHidden
     )
   }, [
     html.div({
