03d8262e8b8ca760b27047da9ffc49fabbd4667c
Irakli Gozalishvili <rfobic@gmail.com>
2016-03-02 15:52:29 -0800

    Update deployment script to deploy to cargo.

--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -14,11 +14,12 @@ DEPLOYED_REV=`curl https://raw.githubusercontent.com/mozilla/browser.html/gh-pag
 if [ "$HEAD" = "$LATEST_REV" ]; then
   if [ "$HEAD" != "$DEPLOYED_REV" ]; then
     echo "Deploy version ${TAG}"
-    echo $HEAD > HEAD
-    git checkout --orphan gh-pages
-    echo "" > .nojekyll
-    rm .gitignore
     npm run build
+    npm run build-cargo
+    cd dist
+    echo $HEAD > HEAD
+    echo "" > .nojekyll
+    git init
     git add .
     git commit -m "Deploy version ${TAG}"
     git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages > /dev/null 2>&1
