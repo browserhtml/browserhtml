5b1430455480b61aa4263b5e414f49198300419c
travis <travis@browser.html>
2016-03-24 18:24:58 -0700

    Include babel-polyfill
    Turns out new babel no longer polyfills Promises by default. It also seems turns out that we don’t have promises on servo. This changes includes polyfill to have Promises in Servo.

--- src/about/newtab/main.js
+++ src/about/newtab/main.js
@@ -4,6 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import "babel-polyfill";
 import {start, Effects} from "reflex";
 import * as NewTab from "./newtab";
 import {Renderer} from "driver";
