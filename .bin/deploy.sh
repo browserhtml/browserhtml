b86bfda29dbf4a17a044b9cc404f6cb52e1677ab
travis <travis@browser.html>
2016-03-25 12:47:22 -0700

    Make deployment script incremental
    Deploying new version no longer wipes out previously deployed version. Fixes servo/servo#10170

--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
 set -e
-set -v
 
 git config --global user.email "travis@browser.html"
 git config --global user.name "travis"
 
 TAG=`git tag -l | sort -g | tail -n 1`
 LATEST_REV=`git rev-parse $TAG`
+REPO_URL=`git config --get remote.origin.url`
 HEAD=`git rev-parse HEAD`
-DEPLOYED_REV=`curl https://raw.githubusercontent.com/browserhtml/browserhtml/gh-pages/HEAD`
+DEPLOYED_REV=`curl --silent https://raw.githubusercontent.com/browserhtml/browserhtml/gh-pages/HEAD`
 
 if [ "$HEAD" = "$LATEST_REV" ]; then
   if [ "$HEAD" != "$DEPLOYED_REV" ]; then
-    echo "Deploy version ${TAG}"
+    echo "Deploying version ${TAG}"
+    rm -rf dist
+    mkdir dist
+    git clone -b gh-pages $REPO_URL dist
+    rm -r dist/*
     npm run build
     npm run build-cargo
     cd dist
     echo $HEAD > HEAD
     echo "" > .nojekyll
-    git init
-    git checkout -b gh-pages
-    git add .
+    git add -A
     git commit -m "Deploy version ${TAG}"
-    git push --force --quiet "https://travis:${GH_TOKEN}@${GH_REF}" gh-pages > /dev/null 2>&1
+    git push --quiet "https://travis:${GH_TOKEN}@${GH_REF}" gh-pages > /dev/null 2>&1
     git status
+    echo "Deployed version ${TAG}"
   else
-    echo "Version ${TAG} is already deployed"
+    echo "Version ${TAG} is already deployed, skip deployment."
   fi
+else
+    echo "Not a tagged commit, skip deployment."
 fi
