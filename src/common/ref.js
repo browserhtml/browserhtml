35d42217338d7a25186b945ea6fb61c262c5f1f8
travis <travis@browser.html>
2016-04-11 22:46:59 -0700

    Create an option for standalone progressbar
    If browser.html is passed query argument `progressbar=standalone` it will not animate progress with regular update loop, instead it will spawn up separate animation frame loop & update element.style directly without causing update or render loops.

---
+++ src/common/ref.js
@@ -0,0 +1,39 @@
+/* @flow */
+
+import {Effects, Task} from "reflex"
+
+/*::
+import type {Never} from "reflex"
+
+export type Model =
+  { name: string
+  , value: string
+  }
+
+export type Action =
+  | Action
+*/
+
+const state =
+  { nextRef: 0
+  };
+
+export const create =
+  ()/*:Model*/ =>
+  ( { name: 'data-ref'
+    , value: `ref-${++state.nextRef}`
+    }
+  );
+
+export const deref =
+  (ref/*:Model*/)/*:Task<Error, HTMLElement>*/ =>
+  new Task
+  ( (succeed, fail) => {
+      const element = document.querySelector(`[${ref.name}='${ref.value}']`);
+      void
+      ( element == null
+      ? fail(Error(`Could not find element by [${ref.name}='${ref.value}'], make sure to include {[ref.name]: ref.value} in a virtual node`))
+      : succeed(element)
+      );
+    }
+  );
