1083d065b2719d880320965c1384d00642c4b155
gozala <rfobic@gmail.com>
2016-05-26 14:17:37 -0700

    Adapt Input module
    - Switch to right positioning instead of left as former caused rendering bugs.
    - Merge type annotations with implementation so they don’t get out of sync.

--- src/browser/Navigators/Navigator/Input.js
+++ src/browser/Navigators/Navigator/Input.js
@@ -6,20 +6,58 @@
 
 import {html, forward, Effects} from 'reflex';
 import {on, focus, selection} from '@driver';
-import {identity} from '../lang/functional';
-import {always, merge} from '../common/prelude';
-import {cursor} from "../common/cursor";
-import {compose, debounce} from '../lang/functional';
-import * as Focusable from '../common/focusable';
-import * as Editable from '../common/editable';
-import * as Keyboard from '../common/keyboard';
-import * as Unknown from '../common/unknown';
-import * as Style from '../common/style';
-
+import {identity} from '../../../lang/functional';
+import {always, merge} from '../../../common/prelude';
+import {cursor} from "../../../common/cursor";
+import {compose, debounce} from '../../../lang/functional';
+import * as Focusable from '../../../common/focusable';
+import * as Editable from '../../../common/editable';
+import * as Keyboard from '../../../common/keyboard';
+import * as Unknown from '../../../common/unknown';
+import * as Style from '../../../common/style';
+import * as Layer from './Layer';
 
 /*::
 import type {Address, DOM} from "reflex"
-import type {Suggestion, Model, Action} from "./input"
+
+export type Flags =
+  { isVisible?: boolean
+  , isFocused?: boolean
+  , value: string
+  }
+
+export type Model =
+  { isVisible: boolean
+  , isFocused: boolean
+  , value: string
+  , selection: ?Editable.Selection
+  }
+
+export type Suggestion =
+  { query: string
+  , match: string
+  , hint: string
+  }
+
+export type Action =
+  | { type: 'Submit' }
+  | { type: 'Query' }
+  | { type: 'Abort' }
+  | { type: 'Enter' }
+  | { type: 'EnterSelection', value: string }
+  | { type: 'Show' }
+  | { type: 'Hide' }
+  | { type: 'SuggestNext' }
+  | { type: 'SuggestPrevious'}
+  | { type: 'Suggest', suggest: Suggestion }
+  | { type: 'Focus' }
+  | { type: 'Blur' }
+  | { type: "Change"
+    , value: string
+    , selection: Editable.Selection
+    }
+  | { type: 'Editable', editable: Editable.Action }
+  | { type: 'Focusable', focusable: Focusable.Action }
 */
 
 // Create a new input submit action.
@@ -27,7 +65,7 @@ export const Query/*:()=>Action*/ = always({ type: 'Query' });
 export const Suggest =
   (suggestion/*:Suggestion*/)/*:Action*/ =>
   ( { type: "Suggest"
-    , source: suggestion
+    , suggest: suggestion
     }
   );
 
@@ -53,14 +91,14 @@ const FocusableAction = action =>
   : action.type === 'Blur'
   ? Blur
   : { type: 'Focusable'
-    , source: action
+    , focusable: action
     }
   );
 
 const EditableAction =
   (action) =>
   ( { type: 'Editable'
-    , source: action
+    , editable: action
     }
   );
 
@@ -95,14 +133,25 @@ const enterSelectionRange = (model, value, start, end) => {
   return [result, Effects.batch([focusFx, editFx])];
 }
 
+const defaultFlags =
+  { isFocused: false
+  , isVisible: false
+  , value: ""
+  }
+
 export const init =
-  ( isVisible/*:boolean*/=false
-  , isFocused/*:boolean*/=false
-  , value/*:string*/=''
-  )/*:[Model, Effects<Action>]*/ =>
-  [ ( { value
-      , isFocused
-      , isVisible
+  (flags/*:Flags*/=defaultFlags)/*:[Model, Effects<Action>]*/ =>
+  [ ( { value: flags.value
+      , isFocused:
+        ( flags.isFocused == null
+        ? false
+        : flags.isFocused
+        )
+      , isVisible:
+        ( flags.isVisible == null
+        ? false
+        : flags.isVisible
+        )
       , selection: null
       }
     )
@@ -121,44 +170,46 @@ const suggest = (model, {query, match, hint}) =>
   )
 
 export const update =
-  (model/*:Model*/, action/*:Action*/)/*:[Model, Effects<Action>]*/ =>
-  ( action.type === 'Abort'
-  ? updateFocusable(model, Focusable.Blur)
-  // We don't really do anything on submit action for now
-  // although in a future we may clear the value or do blur
-  // the input.
-  : action.type === 'Submit'
-  ? [model, Effects.none]
-  : action.type === 'Enter'
-  ? enter(merge(model, {isVisible: true}))
-  : action.type === 'Focus'
-  ? updateFocusable
-    ( merge(model, {isFocused: true, isVisible: true})
-    , action.source
-    )
-  : action.type === 'Blur'
-  ? updateFocusable(model, action.source)
-
-  : action.type === 'EnterSelection'
-  ? enterSelection(merge(model, {isVisible: true}), action.value)
-  : action.type === 'Focusable'
-  ? updateFocusable(model, action.source)
-  : action.type === 'Editable'
-  ? updateEditable(model, action.source)
-  : action.type === 'Change'
-  ? updateEditable(model, Editable.Change(action.value, action.selection))
-  : action.type === 'Show'
-  ? [merge(model, {isVisible: true}), Effects.none]
-  : action.type === 'Hide'
-  ? [merge(model, {isVisible: false}), Effects.none]
-  : action.type === 'SuggestNext'
-  ? [model, Effects.none]
-  : action.type === 'SuggestPrevious'
-  ? [model, Effects.none]
-  : action.type === 'Suggest'
-  ? suggest(model, action.source)
-  : Unknown.update(model, action)
-  );
+  (model/*:Model*/, action/*:Action*/)/*:[Model, Effects<Action>]*/ => {
+    switch (action.type) {
+      case 'Abort':
+        return [merge(model, {isVisible: false}), Effects.none];
+      // We don't really do anything on submit action for now
+      // although in a future we may clear the value or do blur
+      // the input.
+      case 'Submit':
+        return [model, Effects.none];
+      case 'Enter':
+        return enter(merge(model, {isVisible: true}));
+      case 'Focus':
+        return updateFocusable
+        ( merge(model, {isFocused: true, isVisible: true})
+        , Focusable.Focus
+        );
+      case 'Blur':
+        return updateFocusable(model, Focusable.Blur);
+      case 'EnterSelection':
+        return enterSelection(merge(model, {isVisible: true}), action.value);
+      case 'Focusable':
+        return updateFocusable(model, action.focusable);
+      case 'Editable':
+        return updateEditable(model, action.editable);
+      case 'Change':
+        return updateEditable(model, Editable.Change(action.value, action.selection));
+      case 'Show':
+        return [merge(model, {isVisible: true}), Effects.none];
+      case 'Hide':
+        return [merge(model, {isVisible: false}), Effects.none];
+      case 'SuggestNext':
+        return [model, Effects.none];
+      case 'SuggestPrevious':
+        return [model, Effects.none];
+      case 'Suggest':
+        return suggest(model, action.suggest);
+      default:
+        return Unknown.update(model, action)
+    }
+  };
 
 
 const decodeKeyDown = Keyboard.bindings({
@@ -203,11 +254,12 @@ const inputXPadding = 32;
 const style = Style.createSheet({
   combobox: {
     height: inputHeight,
-    left: '50%',
-    marginLeft: `${-1 * (inputWidth / 2)}px`,
+    right: '50%',
+    marginRight: `${-1 * (inputWidth / 2)}px`,
     position: 'absolute',
     top: '40px',
-    width: `${inputWidth}px`
+    width: `${inputWidth}px`,
+    zIndex: Layer.input
   },
   field: {
     backgroundColor: '#EBEEF2',
@@ -264,7 +316,7 @@ const style = Style.createSheet({
 
 export const view =
   (model/*:Model*/, address/*:Address<Action>*/)/*:DOM*/ =>
-  html.div({
+  html.form({
     className: 'input-combobox',
     style: Style.mix
     ( style.combobox
@@ -276,7 +328,7 @@ export const view =
     // Note we submit new query only on `onInput` that's when we expect
     onInput: forward(address, Query)
   }, [
-    html.span({
+    html.figure({
       className: 'input-search-icon',
       style: style.searchIcon
     }, ['']),
