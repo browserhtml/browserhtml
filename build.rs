734268268701c42291d3d4d10fdec096c1fa8fb1
travis <travis@browser.html>
2016-08-12 15:33:22 +0000

    Deploy 31ae5cb94b02a951994895e9aca4bdae357bb16f

--- build.rs
+++ build.rs
@@ -4,14 +4,38 @@
 
 use std::env;
 use std::process::Command;
+use std::path::PathBuf;
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
+    let target = env::var("TARGET").unwrap();
 
-
-    assert!(Command::new("cp")
-            .args(&["-R", "index.html", "css", "components", &out_dir])
-            .status()
-            .unwrap()
-            .success());
+    if target.contains("windows") {
+        // sigh
+        let mut css_dir = PathBuf::from(&out_dir);
+        css_dir.push("css");
+        let mut components_dir = PathBuf::from(&out_dir);
+        components_dir.push("components");
+        assert!(Command::new("xcopy")
+                .args(&["/QY", "index.html", &out_dir])
+                .status()
+                .unwrap()
+                .success());
+        assert!(Command::new("xcopy")
+                .args(&["/EQIY", "components", components_dir.to_str().unwrap()])
+                .status()
+                .unwrap()
+                .success());
+        assert!(Command::new("xcopy")
+                .args(&["/EQIY", "css", css_dir.to_str().unwrap()])
+                .status()
+                .unwrap()
+                .success());
+    } else {
+        assert!(Command::new("cp")
+                .args(&["-R", "index.html", "css", "components", &out_dir])
+                .status()
+                .unwrap()
+                .success());
+    }
 }
