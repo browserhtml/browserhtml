7a9bd8a730369b1bc0a2414e8fcb5e92b5f22d3b
onurtemizkan <onurtemizkan@gmail.com>
2016-09-05 14:26:31 +0300

    Reformat [About/Newtab] to StandardJS Style (#1242)

--- src/About/Newtab/Main.js
+++ src/About/Newtab/Main.js
@@ -4,20 +4,20 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import "babel-polyfill";
-import {start, Effects} from "reflex";
-import * as NewTab from "./Newtab";
-import {Renderer} from "@driver";
+import 'babel-polyfill'
+import { start, Effects } from 'reflex'
+import * as NewTab from './Newtab'
+import { Renderer } from '@driver'
 
 const application = start({
-  flags: void(0),
+  flags: void (0),
   init: NewTab.init,
   update: NewTab.update,
   view: NewTab.view
-});
+})
 
-const renderer = new Renderer({target: document.body});
-application.view.subscribe(renderer.address);
-application.task.subscribe(Effects.driver(application.address));
+const renderer = new Renderer({target: document.body})
+application.view.subscribe(renderer.address)
+application.task.subscribe(Effects.driver(application.address))
 
-window.application = application;
+window.application = application
--- src/About/Newtab/Newtab.js
+++ src/About/Newtab/Newtab.js
@@ -4,140 +4,129 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import {merge, tag, tagged} from "../../Common/Prelude"
-import {Effects, html, thunk, forward} from "reflex"
-import {Style, StyleSheet} from "../../Common/Style";
-import {cursor} from "../../Common/Cursor";
-import * as Unknown from "../../Common/Unknown";
+import {merge} from '../../Common/Prelude'
+import {Effects, html, thunk, forward} from 'reflex'
+import {Style, StyleSheet} from '../../Common/Style'
+import {cursor} from '../../Common/Cursor'
+import * as Unknown from '../../Common/Unknown'
 
-import * as Tiles from './Newtab/Tiles';
-import * as Wallpapers from './Newtab/Wallpapers';
+import * as Tiles from './Newtab/Tiles'
+import * as Wallpapers from './Newtab/Wallpapers'
 
+import type {Address, DOM} from 'reflex'
 
-import type {Address, DOM} from "reflex"
+export type Model = {
+  wallpapers: Wallpapers.Model,
+  tiles: Tiles.Model
+}
 
-export type Model =
-  { wallpapers: Wallpapers.Model
-  , tiles: Tiles.Model
-  }
-
-
-export type Action =
-  | { type: "Wallpapers", wallpapers: Wallpapers.Action }
-  | { type: "Tiles", tiles: Tiles.Action }
-
-
+export type Action = | {
+      type: "Wallpapers",
+      wallpapers: Wallpapers.Action
+    } | {
+      type: "Tiles",
+      tiles: Tiles.Action
+    }
 
 const WallpapersAction =
-  action =>
-  ( { type: 'Wallpapers'
-    , wallpapers: action
-    }
-  );
+  action => ({
+    type: 'Wallpapers',
+    wallpapers: action
+  })
 
 const TilesAction =
-  action =>
-  ( { type: 'Tiles'
-    , tiles: action
-    }
-  );
+  action => ({
+    type: 'Tiles',
+    tiles: action
+  })
 
-export const init =
-  ():[Model, Effects<Action>] =>
-  {
-    const [tiles, tilesFx] = Tiles.init();
-    const [wallpapers, wallpaperFx] = Wallpapers.init();
-    return (
-      [
-        { wallpapers
-        , tiles
-        }
-      , Effects.batch
-        ( [ tilesFx.map(TilesAction)
-          , wallpaperFx.map(WallpapersAction)
-          ]
-        )
-      ]
-    );
-  }
+export const init = ():[Model, Effects<Action>] => {
+  const [tiles, tilesFx] = Tiles.init()
+  const [wallpapers, wallpaperFx] = Wallpapers.init()
+  return ([
+    {
+      wallpapers,
+      tiles
+    },
+    Effects.batch([
+      tilesFx.map(TilesAction),
+      wallpaperFx.map(WallpapersAction)
+    ])
+  ])
+}
 
 const updateWallpapers = cursor({
   get: model => model.wallpapers,
   set: (model, wallpapers) => merge(model, {wallpapers}),
   update: Wallpapers.update,
   tag: WallpapersAction
-});
+})
 
-const updateTiles = cursor
-  ( { get: model => model.tiles
-    , set: (model, tiles) => merge(model, {tiles})
-    , update: Tiles.update
-    , tag: TilesAction
-    }
-  );
+const updateTiles = cursor({
+  get: model => model.tiles,
+  set: (model, tiles) => merge(model, {tiles}),
+  update: Tiles.update,
+  tag: TilesAction
+})
 
-export const update =
-  (model:Model, action:Action):[Model, Effects<Action>] =>
-  ( action.type === 'Wallpapers'
-  ? updateWallpapers(model, action.wallpapers)
-  : action.type === 'Tiles'
-  ? updateTiles(model, action.tiles)
+export const update = (model:Model, action:Action):[Model, Effects<Action>] => (
+  action.type === 'Wallpapers' ? updateWallpapers(model, action.wallpapers)
+  : action.type === 'Tiles' ? updateTiles(model, action.tiles)
   : Unknown.update(model, action)
-  );
+)
 
 export const styleSheet =
-  StyleSheet.create
-  ( { base:
-      { backgroundColor: '#fff'
-      , width: '100%'
-      , height: '100%'
-      , position: 'absolute'
-      , minHeight: '580px'
-      }
-    , shown:
-      {}
-    , hidden:
-      { display: 'none'
-      }
+  StyleSheet.create({
+    base: {
+      backgroundColor: '#fff',
+      width: '100%',
+      height: '100%',
+      position: 'absolute',
+      minHeight: '580px'
+    },
+    shown: {},
+    hidden: {
+      display: 'none'
     }
-  );
+  })
 
-const readWallpaper = ({src, color}) =>
-  (
-    { backgroundImage:
-      ( src
-      ? `url(${src})`
-      : 'none'
-      )
-    , backgroundColor: color
-    , backgroundSize: 'cover'
-    , backgroundRepeat: 'no-repeat'
-    , backgroundPosition: 'center center'
-    }
-  );
+const readWallpaper = ({src, color}) => ({
+  backgroundImage:
+  (src
+  ? `url(${src})`
+  : 'none'),
+  backgroundColor: color,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center center'
+})
 
-export const view =
-  ({wallpapers, tiles, help}:Model, address:Address<Action>):DOM => {
-  const activeWallpaper = Wallpapers.active(wallpapers);
+export const view = ({
+    wallpapers,
+    tiles,
+    help
+  }:Model,
+  address:Address<Action>):DOM => {
+  const activeWallpaper = Wallpapers.active(wallpapers)
   return (
-    html.div
-    ( { className: 'newtab'
-      , style: Style
-        ( styleSheet.base
-        , readWallpaper(activeWallpaper)
-        )
-      }
-    , [ html.meta
-        ( { name: "theme-color"
-          , content: activeWallpaper.color
+    html.div(
+      {
+        className: 'newtab',
+        style: Style(styleSheet.base, readWallpaper(activeWallpaper))
+      },
+      [
+        html.meta(
+          {
+            name: 'theme-color',
+            content: activeWallpaper.color
           }
-        )
-      , thunk
-        ( 'tiles'
-        , Tiles.view
-        , tiles
-        , forward(address, TilesAction)
-        , activeWallpaper.isDark
+        ),
+        thunk(
+          'tiles',
+          Tiles.view,
+          tiles,
+          forward(address, TilesAction),
+          activeWallpaper.isDark
         )
       ]
     )
