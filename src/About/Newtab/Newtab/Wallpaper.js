9420a2706b0c7e873c865328ceca71ceea496774
onurtemizkan <onurtemizkan@gmail.com>
2016-09-26 01:46:38 +0300

    Reformat [About/Newtab/Newtab/Wallpaper.js] to StandardJS Style (#1242)

--- src/About/Newtab/Newtab/Wallpaper.js
+++ src/About/Newtab/Newtab/Wallpaper.js
@@ -4,59 +4,50 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import {Effects, html, thunk, forward} from "reflex"
-import {merge, always, tagged} from "../../../Common/Prelude"
-import * as Style from "../../../Common/Style";
-import * as Unknown from "../../../Common/Unknown";
+import {html, forward} from 'reflex'
+import {always} from '../../../Common/Prelude'
+import * as Style from '../../../Common/Style'
 
-import hardcodedWallpaper from "../Wallpaper.json";
-
-
-import type {Address, DOM} from "reflex"
-import type {URI} from "../../../Common/Prelude"
+import type {Address, DOM} from 'reflex'
+import type {URI} from '../../../Common/Prelude'
 
 export type {URI}
 export type ID = string
 export type Color = string
 
-export type Model =
-  { src: ?URI
-  , color: string
-  , isDark: boolean
-  }
-
+export type Model = {
+  src: ?URI,
+  color: string,
+  isDark: boolean
+}
 
 export type Action =
-  | { type: "Choose" }
+  | { type: 'Choose' }
 
+const Choose:Action = {
+  type: 'Choose'
+}
 
-const Choose:Action =
-  { type: 'Choose'
-  };
-
-const styleSheet = Style.createSheet
-  ( { base:
-      { border: '1px solid rgba(0,0,0,0.15)'
-      , cursor: 'pointer'
-      , borderRadius: '50%'
-      , display: 'inline-block'
-      , width: '10px'
-      , height: '10px'
-      , margin: '0 2px'
-      }
-    }
-  );
+const styleSheet = Style.createSheet({
+  base: {
+    border: '1px solid rgba(0,0,0,0.15)',
+    cursor: 'pointer',
+    borderRadius: '50%',
+    display: 'inline-block',
+    width: '10px',
+    height: '10px',
+    margin: '0 2px'
+  }
+})
 
 export const view =
-  (model:Model, address:Address<Action>):DOM =>
-  ( html.div
-    ( { className: 'wallpaper-choice'
-      , onClick: forward(address, always(Choose))
-      , style: Style.mix
-        ( styleSheet.base
-        , { backgroundColor: model.color
-          }
-        )
-      }
-    )
-  );
+  (model:Model, address:Address<Action>):DOM => (
+    html.div({
+      className: 'wallpaper-choice',
+      onClick: forward(address, always(Choose)),
+      style: Style.mix(
+        styleSheet.base,
+        { backgroundColor: model.color }
+      )
+    })
+  )
