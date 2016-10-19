097d37d3a99e85da1fd6e6d4a5835305ed24cb33
Onur Temizkan <onurtemizkan@gmail.com>
2016-10-20 02:03:53 +0300

    Reformat [About/Repl/] to StandardJS Style (#1242) (#1254)

--- src/About/Repl/Main.js
+++ src/About/Repl/Main.js
@@ -4,33 +4,30 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import "babel-polyfill";
-import {start, Effects} from "reflex";
-import * as UI from "./Repl";
-import {Renderer} from "@driver";
+import 'babel-polyfill'
+import {start, Effects} from 'reflex'
+import * as UI from './Repl'
+import {Renderer} from '@driver'
 
-const isReload = window.application != null;
+const isReload = window.application != null
 
-const restore =
-  () =>
-  [ window.application.model.value
-  , Effects.none
-  ]
+const restore = () => [
+  window.application.model.value,
+  Effects.none
+]
 
 
 const application = start({
   flags: void(0),
-  init:
-  ( isReload
-  ? restore
-  : UI.init
+  init: (
+    isReload ? restore : UI.init
   ),
   update: UI.update,
   view: UI.view
-});
+})
 
-const renderer = new Renderer({target: document.body});
-application.view.subscribe(renderer.address);
-application.task.subscribe(Effects.driver(application.address));
+const renderer = new Renderer({target: document.body})
+application.view.subscribe(renderer.address)
+application.task.subscribe(Effects.driver(application.address))
 
-window.application = application;
+window.application = application
--- src/About/Repl/Repl.js
+++ src/About/Repl/Repl.js
@@ -4,250 +4,224 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import {html, thunk, forward, Effects} from 'reflex';
-import {merge, always, batch, tag, tagged} from "../../Common/Prelude";
-import {Style, StyleSheet} from '../../Common/Style';
-import * as Cell from './Repl/Cell';
-import * as Settings from '../../Common/Settings';
-import * as Unknown from '../../Common/Unknown';
-import * as Host from './Repl/Host';
-import * as Input from "./Repl/Input";
-import * as Output from "./Repl/Output";
+import {html, forward, Effects} from 'reflex'
+import {merge, always, batch} from '../../Common/Prelude'
+import {StyleSheet} from '../../Common/Style'
+import * as Cell from './Repl/Cell'
+import * as Unknown from '../../Common/Unknown'
+import * as Host from './Repl/Host'
+import * as Input from './Repl/Input'
+import * as Output from './Repl/Output'
 
+import {onWindow} from '@driver'
 
-import {onWindow} from "@driver";
+import type {Address, DOM} from 'reflex'
+import type {Integer} from '../../Common/Prelude'
 
-
-import type {Address, DOM} from "reflex"
-import type {Integer} from "../../Common/Prelude"
-
-export type Model =
-  { nextID: Integer
-  , active: Integer
-  , order: [Cell.ID]
-  , cells: {[key:Cell.ID]: Cell.Model}
+export type Model = {
+  nextID: Integer,
+  active: Integer,
+  order: [Cell.ID],
+  cells: {
+    [key: Cell.ID]: Cell.Model
   }
+}
 
 export type Action =
-  | { type: "CreateCell" }
-  | { type: "Focus" }
-  | { type: "Evaluate"
-    , id: Cell.ID
-    , evaluate: Input.Model
-    }
-  | { type: "Print"
-    , id: Cell.ID
-    , print: Output.Model
-    }
-  | { type: "Cell"
-    , id: Cell.ID
-    , cell: Cell.Action
-    }
-
-
-
+  | {type: 'CreateCell'}
+  | {type: 'Focus'}
+  | {
+    type: 'Evaluate',
+    id: Cell.ID,
+    evaluate: Input.Model
+  }
+  | {
+    type: 'Print',
+    id: Cell.ID,
+    print: Output.Model
+  }
+  | {
+    type: 'Cell',
+    id: Cell.ID,
+    cell: Cell.Action
+  }
 
 // Actions
 
-const CreateCell =
-  ( { type: "CreateCell"
+const CreateCell = ({
+  type: 'CreateCell'
+})
+
+const Focus = ({
+  type: 'Focus'
+})
+
+const Evaluate = (id, input) => ({ // eslint-disable-line no-unused-vars
+  type: 'Evaluate',
+  id,
+  source: input
+})
+
+const Print = (id, version) =>
+  result => ({
+    type: 'Print',
+    id,
+    print: {
+      version,
+      result
     }
-  );
+  })
 
-const Focus =
-  ( { type: "Focus"
-    }
-  );
+const ByID = id =>
+  action => CellAction(id, action)
 
-const Evaluate =
-  (id, input) =>
-  ( { type: "Evaluate"
-    , id
-    , source: input
-    }
-  );
-
-const Print =
-  (id, version) =>
-  result =>
-  ( { type: "Print"
-    , id
-    , print:
-      { version
-      , result
-      }
-    }
-  );
-
-
-const ByID =
-  id =>
-  action =>
-  CellAction(id, action);
-
-const CellAction =
-  (id, action) =>
-  ( action.type === "Submit"
-  ? { type: "Evaluate"
-    , id
-    , evaluate: action.submit
-    }
-  : { type: "Cell"
-    , id
-    , cell: action
-    }
-  );
-
-
-export const init =
-  ():[Model, Effects<Action>] =>
-  createCell
-  ( { nextID: 0
-    , order: []
-    , cells: {}
-    , active: -1
-    }
-  );
-
-const createCell =
-  model => {
-    const id = String(model.nextID);
-    const [cell, fx] = Cell.init(id);
-    const state = merge
-      ( model
-      , { nextID: model.nextID + 1
-        , order: [...model.order, id]
-        , active: model.nextID
-        , cells: merge
-          ( model.cells
-          , {[id]: cell}
-          )
-        }
-      );
-
-    return [state, fx.map(ByID(id))];
+const CellAction = (id, action) => (
+  action.type === 'Submit' ? {
+    type: 'Evaluate',
+    id,
+    evaluate: action.submit
   }
+  : {
+    type: 'Cell',
+    id,
+    cell: action
+  }
+)
 
-const updateCell =
-  (model, id, action) =>
-  ( model.cells[id] == null
-  ? [ model
-    , Effects.none
-    ]
+export const init = ():[Model, Effects<Action>] =>
+  createCell({
+    nextID: 0,
+    order: [],
+    cells: {},
+    active: -1
+  })
+
+const createCell = model => {
+  const id = String(model.nextID)
+  const [cell, fx] = Cell.init(id)
+  const state = merge(
+    model,
+    {
+      nextID: model.nextID + 1,
+      order: [...model.order, id],
+      active: model.nextID,
+      cells: merge(
+        model.cells,
+        {[id]: cell}
+      )
+    }
+  )
+  return [state, fx.map(ByID(id))]
+}
+
+const updateCell = (model, id, action) => (
+  model.cells[id] == null ? [
+    model,
+    Effects.none
+  ]
   : swapCell(model, id, Cell.update(model.cells[id], action))
+)
+
+const swapCell = (model, id, [cell, fx]) => {
+  const result = [
+    merge(
+      model,
+      {
+        cells: merge(
+          model.cells,
+          {[id]: cell}
+        )
+      }
+    ),
+    fx.map(ByID(id))
+  ]
+  return result
+}
+
+const focus = model =>
+  updateCell(
+    model,
+    String(model.active),
+    Cell.Edit
   )
 
-const swapCell =
-  (model, id, [cell, fx]) => {
-    const result =
-      [ merge
-        ( model
-        , { cells
-          : merge
-            ( model.cells
-            , { [id]: cell }
-            )
-          }
-        )
-      , fx.map(ByID(id))
-      ]
-
-    return result
-  }
-
-const focus =
-  model =>
-  updateCell
-  ( model
-  , String(model.active)
-  , Cell.Edit
-  );
-
-const evaluate =
-  (model, {id, evaluate: source}) =>
-  [ model
-  , Effects
-    .perform(Host.evaluate(id, source.value))
+const evaluate = (model, {id, evaluate: source}) => [
+  model,
+  Effects.perform(Host.evaluate(id, source.value))
     .map(Print(id, source.version))
-  ];
+]
 
-const isActive =
-  (model, id) =>
-  model.active === id;
+const isActive = (model, id) =>
+  model.active === id
 
-const isLast =
-  (model, id) =>
-  model.order[model.order.length - 1] === id;
+const isLast = (model, id) =>
+  model.order[model.order.length - 1] === id
 
-const print = (model, action) =>
-  ( (isActive(model, action.id) && isLast(model, action.id))
-  ? batch
-    ( update
-    , model
-    , [ CreateCell
-      , action
-      ]
+const print = (model, action) => (
+  (isActive(model, action.id) && isLast(model, action.id))
+  ? batch(
+      update,
+      model,
+      [CreateCell, action]
     )
-  : updateCell
-    ( model
-    , action.id
-    , Cell.Print(action.print)
+  : updateCell(
+      model,
+      action.id,
+      Cell.Print(action.print)
     )
-  );
+)
 
 export const update =
-  (model:Model, action:Action):[Model, Effects<Action>] =>
-  ( action.type === 'Cell'
-  ? updateCell(model, action.id, action.cell)
-  : action.type === 'Evaluate'
-  ? evaluate(model, action)
-  : action.type === 'Print'
-  ? print(model, action)
-  : action.type === 'Focus'
-  ? focus(model)
-  : action.type === 'CreateCell'
-  ? createCell(model)
-  : Unknown.update(model, action)
-  );
-
-
-const styleSheet = StyleSheet.create
-  ( { base:
-      { width: '100%'
-      , height: '100%'
-      , position: 'absolute'
-      , top: '0px'
-      , left: '0px'
-      , color: '#839496'
-      , backgroundColor: '#002b36'
-      , fontSize: '12px'
-      , fontFamily: 'Menlo, Courier, monospace'
-      , lineHeight: '14px'
-      , overflow: 'auto'
-      }
+  (model:Model, action:Action):[Model, Effects<Action>] => {
+    switch (action.type) {
+      case 'Cell':
+        return updateCell(model, action.id, action.cell)
+      case 'Evaluate':
+        return evaluate(model, action)
+      case 'Print':
+        return print(model, action)
+      case 'Focus':
+        return focus(model)
+      case 'CreateCell':
+        return createCell(model)
+      default:
+        return Unknown.update(model, action)
     }
-  );
+  }
+
+const styleSheet = StyleSheet.create({
+  base: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    top: '0px',
+    left: '0px',
+    color: '#839496',
+    backgroundColor: '#002b36',
+    fontSize: '12px',
+    fontFamily: 'Menlo, Courier, monospace',
+    lineHeight: '14px',
+    overflow: 'auto'
+  }
+})
 
 export const view =
-  (model:Model, address:Address<Action>):DOM =>
-  html.div
-  ( { style: styleSheet.base
-    , id: 'repl'
-    , onWindowFocus: onWindow(address, always(Focus))
-    }
-  , [ ...Object
-      .keys(model.cells)
-      .map
-      ( id =>
-        Cell.view
-        ( model.cells[id]
-        , forward(address, ByID(id))
-        )
-      )
-    , html.meta
-      ( { name: 'theme-color'
-        , content: `${styleSheet.base.backgroundColor}|${styleSheet.base.color}`
-        }
-      )
+  (model:Model, address:Address<Action>):DOM => html.div(
+    {
+      style: styleSheet.base,
+      id: 'repl',
+      onWindowFocus: onWindow(address, always(Focus))
+    },
+    [
+      ...Object
+        .keys(model.cells)
+        .map(id => Cell.view(
+          model.cells[id],
+          forward(address, ByID(id))
+        )),
+      html.meta({
+        name: 'theme-color',
+        content: `${styleSheet.base.backgroundColor}|${styleSheet.base.color}`
+      })
     ]
-  );
+  )
